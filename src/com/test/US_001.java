227c55836a5a4fc34beda83fb41b38179fdfdb38
Murat Guney <muratguney@Murats-MBP.fios-router.home>
2022-05-13 15:32:25 -0400

    Adding personel information

--- src/com/test/US_001.java
+++ src/com/test/US_001.java
@@ -4,6 +4,13 @@ public class US_001 {
 
     public static void main(String[] args) {
 
+        int age = 44;
+        String firstName = "Murat";
+        String lastName = "Guney";
+
+        System.out.println("First Name = " + firstName+"\nLast Name = "+lastName+"\nAge = "+age);
+
+
 
     }
 }
